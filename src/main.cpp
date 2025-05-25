972b9b7f4ec5c00be81d3d4f5a471294780a7565
Evgeny Beryozoff <jenyaiu90@gmail.com>
2025-05-25 11:27:45 +0300

    Init
    Создан проект.

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+    ////////////////////////////////////////
+   //      AileScript by SnegirSoft      //
+  //                                    //
+ //  File: main.cpp                    //
+////////////////////////////////////////
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Hello world from " << argv[0] << '!' << std::endl;
+	return 0;
+}
